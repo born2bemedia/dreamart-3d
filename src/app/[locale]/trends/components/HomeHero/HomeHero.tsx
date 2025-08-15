4b1bd55333c6e7eb4e2086a98cfd2ee7947a6942
daniil-b2be <dan.developer@born2be.media>
2025-08-15 19:38:05 +0300

    page

---
+++ src/app/[locale]/trends/components/HomeHero/HomeHero.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { fadeInUp } from '@/shared/lib/helpers/animations';
+
+import styles from './HomeHero.module.scss';
+
+export const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <video
+        src="/videos/trends/hero.mp4"
+        autoPlay
+        muted
+        loop
+        playsInline
+        className={styles.hero__video}
+      />
+      <div className={styles.hero__overlay} />
+      <div className={'_container'}>
+        <div className={styles.hero__content}>
+          <motion.h1
+            variants={fadeInUp}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+          >
+            The Future of 3D Creativity
+          </motion.h1>
+          <motion.p
+            variants={fadeInUp}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+          >
+            At Dreamart 3D, we’re all about staying ahead of the curve. This is where we talk
+            trends, share insights, and keep you updated on everything exciting in the world of 3D
+            modelling, animation, UI/UX design, and video production. Explore the latest innovations
+            and see how they’re shaping the future of digital creativity.
+          </motion.p>
+        </div>
+      </div>
+    </section>
+  );
+};
---
+++ src/featured/trends/model/types.ts
@@ -0,0 +1,7 @@
+export type TrendPreview = {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  slug: string;
+};
